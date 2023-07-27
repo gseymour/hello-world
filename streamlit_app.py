892d8d7a3d554236e3f1c067b9f7fb5554dc65f1
gseymour <gailseymour@live.co.uk>
2023-07-27 19:12:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,10 @@ data_load_state.text("Done! (using st.cache_data)")
 
 st.subheader('Raw data')
 st.write(data)
+
+st.subheader('Number of pickups by hour')
+#Use NumPy to generate a histogram that breaks down pickup times binned by hour:
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+#use Streamlit's st.bar_chart() method to draw this histogram
+st.bar_chart(hist_values)
