1c926089e9032788f9327373db0960bf06113d57
gseymour <gailseymour@live.co.uk>
2023-07-27 17:31:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-mport streamlit as st
+import streamlit as st
 
 st.header('st.latex')
 
@@ -7,3 +7,4 @@ st.latex(r'''
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
+Line-by-l
