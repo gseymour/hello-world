7c18b606b62350ae44614851289ccdc215efe9c0
gseymour <gailseymour@live.co.uk>
2023-07-27 19:15:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,9 @@ st.subheader('Map of all pickups')
 #Use the st.map() function to plot the data:
 st.map(data)
 
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
 
 
